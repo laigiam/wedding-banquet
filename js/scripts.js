524f79339a446e34a24b99b07366280e6d2145cc
laigiam <laigiam@outlook.com>
2025-06-24 01:49:17 +0800

    Version 8.1
    Bug fixes for form

--- js/scripts.js
+++ js/scripts.js
@@ -211,16 +211,12 @@ $(document).ready(function () {
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
             $.post('https://script.google.com/macros/s/AKfycbyZIuibGZvvOgMAPmGdJFpfW3FCa2DOE6h-RCbTy5oEE-By4v2cSdg7zViAIG_UzacC/exec', data)
                 .done(function (data) {
                     console.log(data);
+                    alert(JSON.stringify(data));
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
                     } else {
@@ -232,7 +228,7 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
-        }
+        
     });
 
 });
